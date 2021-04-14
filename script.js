dbdc0904aa80bb1c2af388894808675bfd0eea23
Jason Lam <nyjasonlam@gmail.com>
2021-04-14 18:12:50 -0400

    Added checks for lower-case and upper-case letters

--- script.js
+++ script.js
@@ -16,12 +16,22 @@ function generatePassword() {
     passwordLength == undefined
   ) {
     passwordLength = +prompt("Please enter a valid password length. Enter a number between 8 and 128.");
+    if (passwordLength == null) break;
   }
 
   var lowerCaseLetters = confirm("Do you want LOWER-CASE letters?");
   var upperCaseLetters = confirm("Do you want UPPER-CASE letters?");
   var numerals = confirm("Do you want numbers?");
   var specialCharacters = confirm("Do you want special characters?");
+
+  if (lowerCaseLetters) {
+    possibleCharacters += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (upperCaseLetters) {
+    possibleCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
 }
 
 // Write password to the #password input
