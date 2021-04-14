eb27f4fb155fff408baaf80eb5716a4a6d2b701b
Jason Lam <nyjasonlam@gmail.com>
2021-04-14 18:05:59 -0400

    Added password length prompt using while loop

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  // lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  // upperCaseLetters = lowerCaseLetters.toUpperCase();
+  // specialCharacters = "!@#$%^&*";
+  // numbers = "0123456789";
+
+  possibleCharacters = "";
+  var passwordLength;
+  while (
+    passwordLength < 8 ||
+    passwordLength > 128 ||
+    passwordLength == undefined
+  ) {
+    passwordLength = +prompt("Please enter a valid password length. Enter a number between 8 and 128.");
+  }
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
